4c9e9a6fb225c28dcc3dd2c19109b0f6381eeb1f
gn-67 <gokul.nambiar91@gmail.com>
2026-09-17 20:15:38 -0700

    Add: minimum-number-of-swaps-to-make-the-string-balanced - submission-0

---
+++ Data Structures & Algorithms/minimum-number-of-swaps-to-make-the-string-balanced/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minSwaps(self, s: str) -> int:
+
+        close = 0
+        maxClose = 0
+
+
+        for c in s:
+            if c == "[":
+                close -= 1
+            else:
+                close += 1
+
+            maxClose = max(close, maxClose)
+        
+        return (maxClose + 1) // 2
+        
