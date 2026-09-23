a732d5dfe94387d3d2915639dcea4c637a984724
gn-67 <gokul.nambiar91@gmail.com>
2026-09-23 13:28:06 -0700

    Add: longest-palindrome - submission-3

---
+++ Data Structures & Algorithms/longest-palindrome/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        #maybe we use a hashmap to count
+        #and return all the values that are even
+        #pali can have only one odd in center
+
+        count = collections.Counter(s)
+        print(count)
+        result = 0
+        odds = 0
+
+        for c in count.values():
+            if c % 2 == 0:
+                result += c
+            else:
+                if c - 1 > 0:
+                    result += c - 1
+                odds += 1
+
+
+        return result + (odds > 0)
+
+
+
+
+
+        
+
+        
